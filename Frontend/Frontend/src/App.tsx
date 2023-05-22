f2d9bbb2cf594e11462e0be58f0332ebee79dcbb
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-22 11:56:09 +0300

    Added Header, Home, Data, Register and Profile Pages

---
+++ Frontend/Frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider, useLocation } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Profile } from './Pages/Profile';
+import { Data } from './Pages/Data';
+import { Header } from './components/Header';
+import { Register } from './Pages/Register';
+
+import './App.css';
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />} >
+        <Route index element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/data' element={<Data />} />
+        <Route path='/register' element={<Register />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+const Root = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname !== '/register' && <Header />}
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/Frontend/src/Pages/Data.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { DataRow } from "../components/DataRow";
+
+export const Data = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch('https://localhost:7085/Demo');
+    const jsonData = await response.json();
+    setData(jsonData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const createRow = (item:any, index:any) => {
+    return (
+      <DataRow
+        key={index}
+        symbol={item.symbol}
+        price={item.lastPrice}
+      />
+    );
+  };
+
+  console.log(data);
+
+  return (
+    <div style={{ background: '#293142', minHeight: '80vh' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '40px' }}>
+        <div style={{ width: '600px', background: '#1e242c', padding: '40px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
+          <h1 style={{ textAlign: 'center', marginBottom: '20px', color: '#61dafb', letterSpacing: '2px', textTransform: 'uppercase' }}>Data</h1>
+          {data.map(createRow)}
+        </div>
+      </div>
+    </div>
+  );
+};
