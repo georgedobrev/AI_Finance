bf0b6cb879dc8d27d74603159d62576c1082e709
LuchoStoymirov <lazar-luchezar.stoymirov@blankfactor.com>
2023-05-22 13:41:33 +0300

    Styled Home page

--- Frontend/Frontend/src/Pages/Data.tsx
+++ Frontend/Frontend/src/Pages/Data.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import { DataRow } from "../components/DataRow";
 
@@ -27,7 +28,7 @@ export const Data = () => {
   console.log(data);
 
   return (
-    <div style={{ background: '#293142', minHeight: '80vh' }}>
+    <div style={{ background: '#293142', minHeight: '100vh' }}>
       <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '40px' }}>
         <div style={{ width: '600px', background: '#1e242c', padding: '40px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
           <h1 style={{ textAlign: 'center', marginBottom: '20px', color: '#61dafb', letterSpacing: '2px', textTransform: 'uppercase' }}>Data</h1>
--- Frontend/Frontend/src/Pages/Home.tsx
+++ Frontend/Frontend/src/Pages/Home.tsx
@@ -1,7 +1,13 @@
 export const Home = () => {
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <div style={{ position: 'relative', backgroundImage: 'url(src/images/dark-blue-background-geometric-triangle-y05htl5vjn4jm9bi.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', minHeight: '100vh', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingLeft: '20px' }}>
+      <div style={{ position: 'relative', width: '90%', maxWidth: '400px', background: 'rgba(30, 36, 44, 0.6)', padding: '40px', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', backdropFilter: 'blur(5px)', marginLeft: '5%' }}>
+        <h1 style={{ textAlign: 'left', marginBottom: '30px', color: '#61dafb', letterSpacing: '2px', textTransform: 'uppercase', mixBlendMode: 'difference' }}>Welcome to CashGrab.</h1>
+        <h3 style={{ textAlign: 'left', color: '#fff', lineHeight: '1.5', mixBlendMode: 'difference' }}>
+          Our platform utilizes AI to provide information about current prices of selected Stocks and Companies.
+          The information given must not be taken as financial advice.
+        </h3>
+      </div>
+    </div>
+  );
 }
